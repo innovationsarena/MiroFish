79d74c91023a26b7a6da9dd6c1512afe8b7b1fbe
gr-johan-kivi <johan.kivi@goteborgsregionen.se>
2026-04-07 19:49:24 +0200

    mmm

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     allowedHosts: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5001",
+        target: "http://gs4w84gco4w8wwc44c84kc8o:5001",
         changeOrigin: true,
         secure: false,
       },
