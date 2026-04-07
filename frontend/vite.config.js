a47a05e02cbc90e9f30c05704586c2199d5f5cbb
gr-johan-kivi <johan.kivi@goteborgsregionen.se>
2026-04-07 14:42:35 +0200

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@locales': path.resolve(__dirname, '../locales')
-    }
+      "@": path.resolve(__dirname, "src"),
+      "@locales": path.resolve(__dirname, "../locales"),
+    },
   },
   server: {
     port: 3000,
     open: true,
+    allowedHosts: "mirofish.innovationsarenan.se",
     proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
+      "/api": {
+        target: "http://localhost:5001",
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+        secure: false,
+      },
+    },
+  },
+});
