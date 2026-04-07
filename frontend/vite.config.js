e2a0962e01d1fb50b71ff049e990bf55978bdd4d
gr-johan-kivi <johan.kivi@goteborgsregionen.se>
2026-04-07 14:44:21 +0200

    fix: allowed osts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    allowedHosts: "mirofish.innovationsarenan.se",
+    allowedHosts: ["mirofish.innovationsarenan.se"],
     proxy: {
       "/api": {
         target: "http://localhost:5001",
